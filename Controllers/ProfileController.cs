29ce79d236fed3e5789439b3db8d3a883b34f57d
ICHIZXC <nnaththwathn@gmail.com>
2026-02-22 22:26:18 +0700

    Remove Error actions and unused model usings
    Delete the Error() action and the associated ErrorViewModel dependency from CreateTeamController and ProfileController by removing the Error() method and the now-unused 'using ProfileWeb.Models;' directive in each file. Cleans up dead code and unused imports to simplify the controllers.

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using ProfileWeb.Models;
 
 namespace ProfileWeb.Controllers;
 
@@ -16,9 +15,4 @@ public class ProfileController : Controller
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
 }

5f24164121df3c0ba5ca69efc7be23ae476c8381
Sky92-dev <badinskysky@gmail.com>
2026-03-02 13:18:11 +0700

    Updatemode
    GameInfo

---
+++ Models/GameInfo.cs
@@ -0,0 +1,16 @@
+namespace hateekub.Models
+{public class UserGame
+{
+    public int Id { get; set; }
+
+    public int UserProfileId { get; set; }
+    public UserProfile User { get; set; } = null!;
+
+    public int GameId { get; set; }
+    public Game Game { get; set; } = null!;
+
+    public string? InGameName { get; set; }
+
+    public string? ExtraDataJson { get; set; } // ข้อมูลเฉพาะเกม
+}
+}
